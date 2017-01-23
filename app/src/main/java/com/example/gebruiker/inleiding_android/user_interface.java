8995acb37a2e22d8eeb98d1bf3195c60749c4115
dtmartha <dmartha@roc-dev.com>
2017-01-23 14:54:38 +0100

    project aan git toevoegen

---
+++ app/src/main/java/com/example/gebruiker/inleiding_android/user_interface.java
@@ -0,0 +1,20 @@
+package com.example.gebruiker.inleiding_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class user_interface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_interface);
+
+
+    }
+
+
+
+}
